638bf9360834a1eaa5c0116f45ae23f786944a65
Royal6969 <sergiodiazcampos@gmail.com>
2021-10-04 23:53:22 +0200

    creating the project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.angular.ecommerce',
+  appName: 'angular-ecommerce',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
